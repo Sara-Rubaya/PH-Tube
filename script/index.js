252da48f290ff38ff83181a733810bd415d7ed90
Sara <sara@Saras-MacBook-Air.local>
2025-03-14 23:19:38 +0600

    added more details

--- script/index.js
+++ script/index.js
@@ -14,6 +14,20 @@ function loadVideos(){
     .then((data) => displayVideos(data.videos));
 }
 
+const loadCategoryVideos=(id) =>{
+    
+    const url= `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    console.log(url);
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        const clickedButton =document.getElementById(`btn-${id}`);
+        console.log(clickedButton)
+        displayVideos(data.category)
+    });
+}
+
 
 // {
 //     "category_id": "1001",
@@ -26,7 +40,7 @@ function displayCategories(categories){
         // console.log(cat);
         const categoryDiv=document.createElement("div");
         categoryDiv.innerHTML=`
-        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
         categoryContainer.append(categoryDiv);
 
@@ -54,26 +68,49 @@ function displayCategories(categories){
 
 const displayVideos =(videos)=> {
     const videoContainer = document.getElementById("video-container");
+
+    videoContainer.innerHTML="";
+
+if(video.length==0){
+    videoContainer.innerHTML=`
+     <div class="py-20 col-span-full flex flex-col justify-center items-center text-center">
+            <img class="w-[120px]" src="./Assets/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Oops!!Sorry, There is no content here</h2>
+        </div>
+        `;
+    return;
+}
+
 videos.forEach(video=>{
     console.log(video);
 
     const videoCard = document.createElement("div");
 
     videoCard.innerHTML=`
-    <div class="card bg-base-100  shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+  <div class="card bg-base-100  ">
+            <figure class="relative">
+              <img class="w-full h-[150px] object-cover"
+
+                src="${video.thumbnail}s"
+                alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-white bg-black px-2 text-sm rounded">3hrs 56 min ago</span>
+            </figure> 
+            
+            <div class=" flex gap-3 px-0 py-5" >
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                          <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                      </div>
+                </div>
+                <div class="intro">
+                    <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                    <p class="text-sm text-gray-400 flex gap-1 ">${video.authors[0].profile_name} <img class="w-5 h-5"  src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                    <p class="text-sm text-gray-400">${video.others.views} views</p>
+                </div>
+            </div>
+          </div>
     
     `;
     videoContainer.append(videoCard);
@@ -81,4 +118,3 @@ videos.forEach(video=>{
 
 };
 loadCategories();
-loadVideos();
